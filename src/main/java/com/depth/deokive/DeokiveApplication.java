9df310e158b50e9d6fdc569fe2939a0fc6f5546d
Hooby <hooby11105@naver.com>
2025-10-14 04:15:29 +0900

    chore: Init Configuration

---
+++ src/main/java/com/depth/deokive/DeokiveApplication.java
@@ -0,0 +1,13 @@
+package com.depth.deokive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeokiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeokiveApplication.class, args);
+    }
+
+}
