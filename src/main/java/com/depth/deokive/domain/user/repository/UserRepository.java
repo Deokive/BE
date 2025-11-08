f48c5b171e5fabc4fdeef929de0563c4418e14c1
Hooby <hooby11105@naver.com>
2025-11-08 19:45:38 +0900

    feat: Define User Repository

---
+++ src/main/java/com/depth/deokive/domain/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.depth.deokive.domain.user.repository;
+
+import com.depth.deokive.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String Email);
+    Optional<User> findByNickname(String nickname);
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+}
