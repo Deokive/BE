b8c5e293be533408a9fe556aa1fdb0c07b638414
Hooby <hooby11105@naver.com>
2025-12-15 15:36:37 +0900

    feat: Define FriendMap Entity

---
+++ src/main/java/com/depth/deokive/domain/friend/entity/FriendMap.java
@@ -0,0 +1,53 @@
+package com.depth.deokive.domain.friend.entity;
+
+import com.depth.deokive.common.auditor.TimeBaseEntity;
+import com.depth.deokive.domain.friend.entity.enums.FriendStatus;
+import com.depth.deokive.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Table(
+    name = "friend_map",
+    uniqueConstraints = {
+        @UniqueConstraint(
+                name = "uk_friend_map_user_friend",
+                columnNames = {"user_id", "friend_id"}
+        )
+    },
+    indexes = {
+        @Index(name = "idx_friend_map_user", columnList = "user_id")
+    }
+)
+public class FriendMap extends TimeBaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private FriendStatus friendStatus;
+
+    @Column
+    private LocalDateTime acceptedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "friend_id", nullable = false)
+    private User friend;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "requested_by", nullable = false)
+    private User requestedBy;
+}
