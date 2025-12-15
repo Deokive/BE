5389c983e42cbb17f292e500b2e4fc8eec375d5e
Hooby <hooby11105@naver.com>
2025-12-15 14:50:06 +0900

    feat: Define Sticker Entity

---
+++ src/main/java/com/depth/deokive/domain/archive/entity/Sticker.java
@@ -0,0 +1,32 @@
+package com.depth.deokive.domain.archive.entity;
+
+import com.depth.deokive.domain.archive.entity.enums.StickerType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Table(name= "sticker")
+public class Sticker {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    StickerType stickerType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "archive_id", nullable = false)
+    private Archive archive;
+}
