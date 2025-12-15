b45c6e5ebdc6f39f8e87205afe60ed430af9de06
Hooby <hooby11105@naver.com>
2025-12-16 05:39:17 +0900

    Define DiaryFileMapRepository

---
+++ src/main/java/com/depth/deokive/domain/diary/repository/DiaryFileMapRepository.java
@@ -0,0 +1,8 @@
+package com.depth.deokive.domain.diary.repository;
+
+import com.depth.deokive.domain.diary.entity.DiaryFileMap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiaryFileMapRepository extends JpaRepository<DiaryFileMap, Long> {
+}
+
