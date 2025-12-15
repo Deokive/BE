4a07a9989d9a6d20b5895745d2e44cccc50f3370
Hooby <hooby11105@naver.com>
2025-12-16 05:39:17 +0900

    Define ArchiveFileMapRepository

---
+++ src/main/java/com/depth/deokive/domain/archive/repository/ArchiveFileMapRepository.java
@@ -0,0 +1,8 @@
+package com.depth.deokive.domain.archive.repository;
+
+import com.depth.deokive.domain.archive.entity.ArchiveFileMap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArchiveFileMapRepository extends JpaRepository<ArchiveFileMap, Long> {
+}
+
