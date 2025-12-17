030c67968d7c9bc786cc67e8d981bce43facd9e8
송성호 <thdtjdgh1679@naver.com>
2025-12-18 03:48:44 +0900

    feat: ArchiveRepository -> ArchiveRepositoryCustom 상속 추가

--- src/main/java/com/depth/deokive/domain/archive/repository/ArchiveRepository.java
+++ src/main/java/com/depth/deokive/domain/archive/repository/ArchiveRepository.java
@@ -3,6 +3,6 @@ package com.depth.deokive.domain.archive.repository;
 import com.depth.deokive.domain.archive.entity.Archive;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArchiveRepository extends JpaRepository<Archive, Long> {
+public interface ArchiveRepository extends JpaRepository<Archive, Long>, ArchiveRepositoryCustom {
 }
 
