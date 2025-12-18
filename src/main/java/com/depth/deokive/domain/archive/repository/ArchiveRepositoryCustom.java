526a39d72ca4510eb5e8b6f38f9b2a475c691e9e
송성호 <thdtjdgh1679@naver.com>
2025-12-18 18:12:41 +0900

    chore: ArchiveRepositoryCustom 내 아카이브에 맞게 수정

--- src/main/java/com/depth/deokive/domain/archive/repository/ArchiveRepositoryCustom.java
+++ src/main/java/com/depth/deokive/domain/archive/repository/ArchiveRepositoryCustom.java
@@ -1,9 +1,10 @@
 package com.depth.deokive.domain.archive.repository;
 
-import com.depth.deokive.domain.archive.dto.ArchiveResponseDto;
+import com.depth.deokive.domain.archive.dto.ArchiveMeResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ArchiveRepositoryCustom {
-    Page<ArchiveResponseDto> searchArchiveList(Pageable pageable, Long userId);
+    // 내 아카이브 조회(Visibility 무시)
+    Page<ArchiveMeResponseDto> searchMyArchive(Pageable pageable, Long userId);
 }
