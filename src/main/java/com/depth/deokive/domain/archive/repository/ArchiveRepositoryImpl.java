23be654e778a02a1437c6525f7cbf029ac12c3dc
송성호 <thdtjdgh1679@naver.com>
2025-12-18 18:13:15 +0900

    chore: ArchiveRepositoryImpl 내 아카이브에 맞게 수정

--- src/main/java/com/depth/deokive/domain/archive/repository/ArchiveRepositoryImpl.java
+++ src/main/java/com/depth/deokive/domain/archive/repository/ArchiveRepositoryImpl.java
@@ -1,6 +1,6 @@
 package com.depth.deokive.domain.archive.repository;
 
-import com.depth.deokive.domain.archive.dto.ArchiveResponseDto;
+import com.depth.deokive.domain.archive.dto.ArchiveMeResponseDto;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
@@ -28,34 +28,25 @@ public class ArchiveRepositoryImpl implements ArchiveRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Page<ArchiveResponseDto> searchArchiveList(Pageable pageable, Long userId) {
-        // 1. 데이터 조회
-        List<ArchiveResponseDto> content = queryFactory
-                .select(Projections.constructor(ArchiveResponseDto.class,
+    public Page<ArchiveMeResponseDto> searchMyArchive(Pageable pageable, Long userId) {
+        // 1. 컨텐츠 조회 (Projections를 사용해서 DTO로 조회)
+        List<ArchiveMeResponseDto> content = queryFactory
+                .select(Projections.fields(ArchiveMeResponseDto.class,
                         archive.id,
                         archive.title,
-                        user.nickname,
-                        file.filePath,
-                        archiveLikeCount.likeCount.coalesce(0L),
-                        archiveViewCount.viewCount.coalesce(0L),
-                        userId != null ? archiveLike.id.isNotNull() : com.querydsl.core.types.dsl.Expressions.asBoolean(false),
-                        archive.createdAt
-                        ))
+                        file.filePath.as("thumbnail"), // File 엔티티 URL 매핑
+                        archive.createdAt,
+                        archive.lastModifiedAt
+                ))
                 .from(archive)
-                .join(archive.user, user)
-                // 1-1. 좋아요 개수 Join
-                .leftJoin(archiveLikeCount).on(archiveLikeCount.archiveId.eq(archive.id))
-                // 1-2. 조회수 Join
-                .leftJoin(archiveViewCount).on(archiveViewCount.archiveId.eq(archive.id))
-                // 1-3. 썸네일 Join
+                // 썸네일 Join (N+1 해결 -> 필요한 필드만 select해서)
                 .leftJoin(archiveFileMap).on(
                         archiveFileMap.archive.eq(archive)
-                                .and(archiveFileMap.sequence.eq(1))
+                                .and(archiveFileMap.sequence.eq(1)) // 썸네일만
                 )
-                // 1-4. 좋아요 여부 Join
-                .leftJoin(archiveLike).on(
-                        archiveLike.archive.eq(archive)
-                                .and(userId != null ? archiveLike.user.id.eq(userId) : archiveLike.user.id.isNull())
+                .leftJoin(archiveFileMap.file, file)
+                .where(
+                        archive.user.id.eq(userId) // 내 아카이브만
                 )
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
@@ -65,23 +56,26 @@ public class ArchiveRepositoryImpl implements ArchiveRepositoryCustom {
         // 2. 카운트 쿼리
         JPAQuery<Long> countQuery = queryFactory
                 .select(archive.count())
-                .from(archive);
+                .from(archive)
+                .where(archive.user.id.eq(userId));
 
         return new PageImpl<>(content, pageable, countQuery.fetchOne());
     }
 
-    // 정렬 조건(최신순, 인기순, 조회순)
+    // 정렬 조건
     private OrderSpecifier<?> getOrderSpecifier(Pageable pageable) {
         if(!pageable.getSort().isEmpty()) {
             for(Sort.Order order : pageable.getSort()) {
                 Order direction = order.getDirection().isAscending() ? Order.ASC : Order.DESC;
 
                 switch(order.getProperty()) {
+                    /*
                     case "popular": // 인기순
                         return new OrderSpecifier<>(direction, archiveLikeCount.likeCount);
                     case "view": // 조회순
                         return new OrderSpecifier<>(direction, archiveViewCount.viewCount);
-                    case "latest": // 최신순
+                     */
+                    case "createdAt": // 최신순
                         return new OrderSpecifier<>(direction, archive.createdAt);
                 }
             }
