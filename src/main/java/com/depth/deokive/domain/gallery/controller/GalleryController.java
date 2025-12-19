c47428eb0574769b3c0b9b790142e1fdab26594a
Hooby <hooby11105@naver.com>
2025-12-19 21:04:39 +0900

    refactor: 임시 주석 처리->리팩터링 과정에서 방해되기 때문

--- src/main/java/com/depth/deokive/domain/gallery/controller/GalleryController.java
+++ src/main/java/com/depth/deokive/domain/gallery/controller/GalleryController.java
@@ -1,31 +1,31 @@
-package com.depth.deokive.domain.gallery.controller;
-
-import com.depth.deokive.domain.gallery.dto.GalleryResponseDto;
-import com.depth.deokive.domain.gallery.repository.GalleryRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/v1/gallery")
-public class GalleryController {
-
-    private final GalleryRepository galleryRepository;
-
-    @GetMapping("/{archiveId}")
-    public ResponseEntity<Page<GalleryResponseDto>> getGalleries(
-            @PathVariable Long archiveId,
-            @PageableDefault(size = 5) Pageable pageable
-            ) {
-        return ResponseEntity.ok(
-                galleryRepository.searchGalleries(archiveId, pageable)
-        );
-    }
-}
+// package com.depth.deokive.domain.gallery.controller;
+//
+// import com.depth.deokive.domain.gallery.dto.GalleryDto;
+// import com.depth.deokive.domain.gallery.repository.GalleryRepository;
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.data.domain.Page;
+// import org.springframework.data.domain.Pageable;
+// import org.springframework.data.web.PageableDefault;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+//
+// @RestController
+// @RequiredArgsConstructor
+// @RequestMapping("/api/v1/gallery")
+// public class GalleryController {
+//
+//     private final GalleryRepository galleryRepository;
+//
+//     @GetMapping("/{archiveId}")
+//     public ResponseEntity<Page<GalleryDto.Response>> getGalleries(
+//             @PathVariable Long archiveId,
+//             @PageableDefault(size = 5) Pageable pageable
+//             ) {
+//         return ResponseEntity.ok(
+//                 galleryRepository.searchGalleries(archiveId, pageable)
+//         );
+//     }
+// }
--- src/main/java/com/depth/deokive/domain/gallery/repository/GalleryQueryRepository.java
+++ src/main/java/com/depth/deokive/domain/gallery/repository/GalleryQueryRepository.java
@@ -1,60 +1,57 @@
-package com.depth.deokive.domain.gallery.repository;
-
-import com.depth.deokive.domain.file.entity.enums.MediaRole;
-import com.depth.deokive.domain.gallery.dto.GalleryResponseDto;
-import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-
-import java.util.List;
-
-import static com.depth.deokive.domain.gallery.entity.QGallery.gallery;
-import static com.depth.deokive.domain.gallery.entity.QGalleryFileMap.galleryFileMap;
-import static com.depth.deokive.domain.file.entity.QFile.file;
-
-@RequiredArgsConstructor
-public class GalleryQueryRepository {
-
-    private final JPAQueryFactory queryFactory;
-
-    public Page<GalleryResponseDto> searchGalleries(Long archiveId, Pageable pageable) {
-
-        // 1. 컨텐츠 조회
-        // Gallery -> GalleryFileMap -> File 순으로 조인(N+1 문제를 해결하기 위해서)
-        List<GalleryResponseDto> content = queryFactory
-                .select(Projections.constructor(GalleryResponseDto.class,
-                        gallery.id,
-                        gallery.title,
-                        file.filePath,
-                        gallery.createdAt
-                ))
-                .from(gallery)
-                .leftJoin(galleryFileMap).on(
-                        galleryFileMap.gallery.id.eq(gallery.id)
-                                .and(galleryFileMap.mediaRole.eq(MediaRole.PREVIEW)) // 대표 이미지만
-                )
-                .leftJoin(file).on(galleryFileMap.file.id.eq(file.id))
-                .where(
-                        gallery.archive.id.eq(archiveId) // 특정 아카이브에서 조회
-                )
-                .orderBy(gallery.createdAt.desc()) // 최신 순으로
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetch();
-
-        // 2. 카운트 쿼리
-        Long total = queryFactory
-                .select(gallery.count())
-                .from(gallery)
-                .where(gallery.archive.id.eq(archiveId))
-                .fetchOne();
-
-        return new PageImpl<>(content, pageable, total != null ? total : 0L);
-    }
-
-
-}
-
+// package com.depth.deokive.domain.gallery.repository;
+//
+// import com.depth.deokive.domain.file.entity.enums.MediaRole;
+// import com.querydsl.core.types.Projections;
+// import com.querydsl.jpa.impl.JPAQueryFactory;
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.data.domain.Page;
+// import org.springframework.data.domain.PageImpl;
+// import org.springframework.data.domain.Pageable;
+//
+// import java.util.List;
+//
+// import static com.depth.deokive.domain.gallery.entity.QGallery.gallery;
+// import static com.depth.deokive.domain.gallery.entity.QGalleryFileMap.galleryFileMap;
+// import static com.depth.deokive.domain.file.entity.QFile.file;
+//
+// @RequiredArgsConstructor
+// public class GalleryQueryRepository {
+//
+//     private final JPAQueryFactory queryFactory;
+//
+//     public Page<GalleryResponseDto> searchGalleries(Long archiveId, Pageable pageable) {
+//
+//         // 1. 컨텐츠 조회
+//         // Gallery -> GalleryFileMap -> File 순으로 조인(N+1 문제를 해결하기 위해서)
+//         List<GalleryResponseDto> content = queryFactory
+//                 .select(Projections.constructor(GalleryResponseDto.class,
+//                         gallery.id,
+//                         gallery.title,
+//                         file.filePath,
+//                         gallery.createdAt
+//                 ))
+//                 .from(gallery)
+//                 .leftJoin(galleryFileMap).on(
+//                         galleryFileMap.gallery.id.eq(gallery.id)
+//                                 .and(galleryFileMap.mediaRole.eq(MediaRole.PREVIEW)) // 대표 이미지만
+//                 )
+//                 .leftJoin(file).on(galleryFileMap.file.id.eq(file.id))
+//                 .where(
+//                         gallery.archive.id.eq(archiveId) // 특정 아카이브에서 조회
+//                 )
+//                 .orderBy(gallery.createdAt.desc()) // 최신 순으로
+//                 .offset(pageable.getOffset())
+//                 .limit(pageable.getPageSize())
+//                 .fetch();
+//
+//         // 2. 카운트 쿼리
+//         Long total = queryFactory
+//                 .select(gallery.count())
+//                 .from(gallery)
+//                 .where(gallery.archive.id.eq(archiveId))
+//                 .fetchOne();
+//
+//         return new PageImpl<>(content, pageable, total != null ? total : 0L);
+//     }
+// }
+//
