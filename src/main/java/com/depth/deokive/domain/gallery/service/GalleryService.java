da73520e2f35b159ec343d302fea98a0b55cfffc
Hooby <hooby11105@naver.com>
2025-12-20 06:50:22 +0900

    feat: Define GalleryService

---
+++ src/main/java/com/depth/deokive/domain/gallery/service/GalleryService.java
@@ -0,0 +1,34 @@
+package com.depth.deokive.domain.gallery.service;
+
+import com.depth.deokive.domain.archive.entity.Archive;
+import com.depth.deokive.domain.archive.repository.ArchiveRepository;
+import com.depth.deokive.domain.gallery.dto.GalleryDto;
+import com.depth.deokive.domain.gallery.repository.GalleryQueryRepository;
+import com.depth.deokive.system.config.aop.ExecutionTime;
+import com.depth.deokive.system.exception.model.ErrorCode;
+import com.depth.deokive.system.exception.model.RestException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class GalleryService {
+
+    private final ArchiveRepository archiveRepository;
+    private final GalleryQueryRepository galleryQueryRepository;
+
+    @ExecutionTime
+    @Transactional(readOnly = true)
+    public GalleryDto.PageListResponse getGalleries(Long archiveId, Pageable pageable) {
+
+        Archive archive = archiveRepository.findById(archiveId)
+                .orElseThrow(() -> new RestException(ErrorCode.ARCHIVE_NOT_FOUND)); // 예외처리는 프로젝트에 맞게
+
+        Page<GalleryDto.Response> page = galleryQueryRepository.searchGalleriesByArchive(archiveId, pageable);
+
+        return GalleryDto.PageListResponse.of(archive.getTitle(), page);
+    }
+}
