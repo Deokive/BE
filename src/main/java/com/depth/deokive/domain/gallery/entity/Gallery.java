150ed7ebd2c1637c11f7da69dc705a19549e4a06
Hooby <hooby11105@naver.com>
2025-12-19 21:02:22 +0900

    refactor: Gallery Domain -> GalleryBook 두고 GalleryFileMap 제거

--- src/main/java/com/depth/deokive/domain/gallery/entity/Gallery.java
+++ src/main/java/com/depth/deokive/domain/gallery/entity/Gallery.java
@@ -1,7 +1,7 @@
 package com.depth.deokive.domain.gallery.entity;
 
 import com.depth.deokive.common.auditor.TimeBaseEntity;
-import com.depth.deokive.domain.archive.entity.Archive;
+import com.depth.deokive.domain.file.entity.File;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,10 +18,14 @@ public class Gallery extends TimeBaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String title;
-
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "archive_id", nullable = false)
-    private Archive archive;
+    @JoinColumn(name = "gallery_book_id", nullable = false)
+    private GalleryBook galleryBook;
+
+    @ManyToOne(fetch = FetchType.LAZY) // 파일 재사용성을 위해 OneToOne 대신 ManyToOne 권장
+    @JoinColumn(name = "file_id", nullable = false)
+    private File file;
+
+    // @Column(nullable = false)
+    // private Integer sequence; // 앨범 내 정렬 순서 (사용자 지정) // TODO: 기획측에서 아직 의도는 안했지만, 필요할 가능성이 높음 (추후 확장)
 }
