ae1cf728cf518d3876d8f1dd68be289679d54c44
Hooby <hooby11105@naver.com>
2025-12-19 21:44:15 +0900

    refactor: Remove GalleryResponeDto, Create GalleryDto.Response for convention

---
+++ src/main/java/com/depth/deokive/domain/gallery/dto/GalleryDto.java
@@ -0,0 +1,17 @@
+package com.depth.deokive.domain.gallery.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+public class GalleryDto {
+    @Data @Builder @NoArgsConstructor @AllArgsConstructor
+    public static class Response {
+        private Long id; // 갤러리 삭제하거나 수정하고 싶을 수 있으니까
+        private String thumbnailUrl;
+        private LocalDateTime createdAt;
+    }
+}
