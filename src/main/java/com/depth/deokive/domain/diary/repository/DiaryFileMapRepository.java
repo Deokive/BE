262caf13c0bd5b7d9674255b856eaf30e1f54de3
Hooby <hooby11105@naver.com>
2025-12-21 07:36:55 +0900

    feat: Define DiaryFileMapRepository (Custom Method for bulk delete)

--- src/main/java/com/depth/deokive/domain/diary/repository/DiaryFileMapRepository.java
+++ src/main/java/com/depth/deokive/domain/diary/repository/DiaryFileMapRepository.java
@@ -2,7 +2,16 @@ package com.depth.deokive.domain.diary.repository;
 
 import com.depth.deokive.domain.diary.entity.DiaryFileMap;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DiaryFileMapRepository extends JpaRepository<DiaryFileMap, Long> {
+    List<DiaryFileMap> findAllByDiaryIdOrderBySequenceAsc(Long diaryId);
+
+    @Modifying
+    @Query("DELETE FROM DiaryFileMap dfm WHERE dfm.diary.id = :diaryId")
+    void deleteAllByDiaryId(Long diaryId);
 }
 
