d13230c3f7d1debf7cae22e7e5670fd636ba8ff9
Hooby <hooby11105@naver.com>
2025-12-21 07:34:12 +0900

    feat: Diary->add Update Method

--- src/main/java/com/depth/deokive/domain/diary/entity/Diary.java
+++ src/main/java/com/depth/deokive/domain/diary/entity/Diary.java
@@ -2,6 +2,7 @@ package com.depth.deokive.domain.diary.entity;
 
 import com.depth.deokive.common.auditor.UserBaseEntity;
 import com.depth.deokive.domain.archive.entity.enums.Visibility;
+import com.depth.deokive.domain.diary.dto.DiaryDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -41,4 +42,19 @@ public class Diary extends UserBaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "diary_book_id", nullable = false)
     private DiaryBook diaryBook;
+
+    public void update(DiaryDto.Request request) {
+        if (request == null) return;
+
+        this.title = nonBlankOrDefault(request.getTitle(), this.title);
+        this.content = nonBlankOrDefault(request.getContent(), this.content);
+        this.recordedAt = nonBlankOrDefault(request.getRecordedAt(), this.recordedAt);
+        this.color = nonBlankOrDefault(request.getColor(), this.color);
+        this.visibility = nonBlankOrDefault(request.getVisibility(), this.visibility);
+    }
+
+    // TODO: 사실 이게 PATCH 패턴 처리 방식. Validation 에서 체크를 해줘서 빈 값 들어올 일은 없긴 한데... 일단 보류. 리팩토링 단계에서 고려
+    private <T> T nonBlankOrDefault(T newValue, T currentValue) {
+        return newValue != null ? newValue : currentValue;
+    }
 }
