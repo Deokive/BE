fde0cddce22636b8b71eef91268ee674f940db19
송성호 <thdtjdgh1679@naver.com>
2025-12-21 02:07:44 +0900

    feat: 핫피드 페이지네이션에 필요한 로직 및 주석 추가

--- src/main/java/com/depth/deokive/domain/archive/service/ArchiveService.java
+++ src/main/java/com/depth/deokive/domain/archive/service/ArchiveService.java
@@ -22,34 +22,69 @@ public class ArchiveService {
     private final FriendMapRepository friendMapRepository;
     private final UserRepository userRepository;
 
+    // 내 아카이브 목록 조회
     @ExecutionTime
     @Transactional(readOnly = true)
     public ArchiveDto.PageListResponse getMyArchives(Long userId, Pageable pageable) {
+        // 1. 조회
         Page<ArchiveDto.Response> page = archiveQueryRepository.searchArchives(userId, true, pageable);
+
+        // 2. 인덱스 범위 검증
+        validateIndexBounds(pageable, page);
+
         return ArchiveDto.PageListResponse.of(page);
     }
 
+    // 친구 아카이브 목록 조회
     @ExecutionTime
     @Transactional(readOnly = true)
     public ArchiveDto.PageListResponse getFriendArchives(Long myUserId, Long friendId, Pageable pageable) {
-
+        // 1. 친구 관계 검증
         validateFriendRelationship(myUserId, friendId);
 
+        // 2. 조회
         Page<ArchiveDto.Response> page = archiveQueryRepository.searchArchives(friendId, false, pageable);
+
+        // 3. 인덱스 범위 검증
+        validateIndexBounds(pageable, page);
+
+        return ArchiveDto.PageListResponse.of(page);
+    }
+
+    // 핫피드 목록 조회
+    @ExecutionTime
+    @Transactional(readOnly = true)
+    public ArchiveDto.PageListResponse getHotArchives(Pageable pageable) {
+
+        // 1. 핫피드 조회
+        Page<ArchiveDto.Response> page = archiveQueryRepository.searchHotArchives(pageable);
+
+        // 2. 페이지 범위 검증(데이터 없는데 페이지 요청 시 -> 404에러)
+        validateIndexBounds(pageable, page);
 
         return ArchiveDto.PageListResponse.of(page);
     }
 
     // 친구 관계 검증 로직 분리 (Clean Code)
     private void validateFriendRelationship(Long myUserId, Long friendId) {
+        // 친구 존재 여부 확인
         if (!userRepository.existsById(friendId)) {
-            throw new RestException(ErrorCode.USER_NOT_FOUND);
+            throw new RestException(ErrorCode.USER_NOT_FOUND); // 존재하지 X -> 404에러
         }
 
+        // 친구 관계 확인(ACCEPTED인지)
         boolean isFriend = friendMapRepository.existsFriendship(myUserId, friendId, FriendStatus.ACCEPTED);
 
         if (!isFriend) {
-            throw new RestException(ErrorCode.AUTH_FORBIDDEN); // 친구가 아님
+            throw new RestException(ErrorCode.AUTH_FORBIDDEN); // 친구 X -> 403 에러
+        }
+    }
+
+    // 공통 검증 로직(원래 있으면 재사용, 없으면 추가)
+    public void validateIndexBounds(Pageable pageable, Page<?> pageData) {
+        // 요청한 페이지 -> 전체 페이지 수, 데이터가 아예 없는게 아니면 404 예외처리
+        if(pageable.getPageNumber() > 0 && pageData.getTotalPages() <= pageable.getPageNumber()) {
+            throw new RestException(ErrorCode.DB_DATA_NOT_FOUND);
         }
     }
 }
