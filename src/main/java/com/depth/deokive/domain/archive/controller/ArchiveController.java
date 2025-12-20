5669e37e5d8b16252918ee46b43b7ff0adca4f02
송성호 <thdtjdgh1679@naver.com>
2025-12-21 02:09:00 +0900

    feat: 핫피드 페이지네이션 매핑 구현 및 주석 추가

--- src/main/java/com/depth/deokive/domain/archive/controller/ArchiveController.java
+++ src/main/java/com/depth/deokive/domain/archive/controller/ArchiveController.java
@@ -22,7 +22,7 @@ public class ArchiveController {
     @GetMapping("/me")
     @Operation(summary = "내 아카이브 목록 조회")
     public ResponseEntity<ArchiveDto.PageListResponse> getMyArchives(
-            @AuthenticationPrincipal UserPrincipal userPrincipal,
+            @AuthenticationPrincipal UserPrincipal userPrincipal, // 로그인 필수
             @Valid @ModelAttribute ArchiveDto.ArchivePageRequest pageRequest
     ) {
         ArchiveDto.PageListResponse response =
@@ -34,12 +34,24 @@ public class ArchiveController {
     @GetMapping("/friend/{friendId}")
     @Operation(summary = "친구 아카이브 목록 조회")
     public ResponseEntity<ArchiveDto.PageListResponse> getFriendArchives(
-            @AuthenticationPrincipal UserPrincipal userPrincipal,
+            @AuthenticationPrincipal UserPrincipal userPrincipal, // 로그인 필수
             @PathVariable Long friendId,
             @Valid @ModelAttribute ArchiveDto.ArchivePageRequest pageRequest
     ) {
         ArchiveDto.PageListResponse response =
                 archiveService.getFriendArchives(userPrincipal.getUserId(), friendId, pageRequest.toPageable());
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/hot")
+    @Operation(summary = "핫피드 목록 조회", description = "핫피드 목록을 조회")
+    public ResponseEntity<ArchiveDto.PageListResponse> getHotArchives(
+            // 비로그인도 가능 -> USerPrincipal 필요 X
+            @Valid @ModelAttribute ArchiveDto.ArchivePageRequest pageRequest
+    ) {
+        ArchiveDto.PageListResponse response =
+                archiveService.getHotArchives(pageRequest.toPageable());
 
         return ResponseEntity.ok(response);
     }
