b6f4ff7af8aca4648a112e67249aafa4a7ed8126
Hooby <hooby11105@naver.com>
2025-12-26 00:52:43 +0900

    refactor: friendship checker method (JPQL to JPA)

--- src/main/java/com/depth/deokive/domain/friend/repository/FriendMapRepository.java
+++ src/main/java/com/depth/deokive/domain/friend/repository/FriendMapRepository.java
@@ -8,11 +8,5 @@ import org.springframework.data.repository.query.Param;
 
 public interface FriendMapRepository extends JpaRepository<FriendMap, Long> {
 
-    // 친구 관계 존재 여부 확인 (JPQL로 명확하게 정의)
-    @Query("SELECT CASE WHEN COUNT(f) > 0 THEN true ELSE false END " +
-            "FROM FriendMap f " +
-            "WHERE f.user.id = :userId AND f.friend.id = :friendId AND f.friendStatus = :status")
-    boolean existsFriendship(@Param("userId") Long userId,
-                             @Param("friendId") Long friendId,
-                             @Param("status") FriendStatus status);
+    boolean existsByUserIdAndFriendIdAndFriendStatus(Long userId, Long friendId, FriendStatus status);
 }
