23242615b9026311cd122e62044b63ab21ba24d1
Hooby <hooby11105@naver.com>
2025-12-26 00:38:05 +0900

    refactor: Remove Unused Repositories by removed tables

--- src/main/java/com/depth/deokive/domain/archive/repository/ArchiveLikeCountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.depth.deokive.domain.archive.repository;
-
-import com.depth.deokive.domain.archive.entity.ArchiveLikeCount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArchiveLikeCountRepository extends JpaRepository<ArchiveLikeCount, Long> {
-}
-
