5d5957190f2b3fa47438b387ba69517a80bee7b2
송성호 <thdtjdgh1679@naver.com>
2025-12-27 20:05:53 +0900

    feat: TicketBookRepository에 FetchJoin 쿼리 추가

--- src/main/java/com/depth/deokive/domain/ticket/repository/TicketBookRepository.java
+++ src/main/java/com/depth/deokive/domain/ticket/repository/TicketBookRepository.java
@@ -2,6 +2,15 @@ package com.depth.deokive.domain.ticket.repository;
 
 import com.depth.deokive.domain.ticket.entity.TicketBook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface TicketBookRepository extends JpaRepository<TicketBook, Long> {
+    @Query("SELECT tb FROM TicketBook tb " +
+            "JOIN FETCH tb.archive a " +
+            "JOIN FETCH a.user u " +
+            "WHERE tb.id = :id")
+    Optional<TicketBook> findByIdWithArchiveAndUser(@Param("id") Long id);
 }
