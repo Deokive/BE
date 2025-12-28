88b7ec062855f66b0ad7edefa71e2c745a322e6c
Hooby <hooby11105@naver.com>
2025-12-29 00:54:51 +0900

    fix: Remove Enetities and Repository because of denormalization fields

--- src/main/java/com/depth/deokive/domain/post/repository/PostLikeCountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.depth.deokive.domain.post.repository;
-
-import com.depth.deokive.domain.post.entity.PostLikeCount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostLikeCountRepository extends JpaRepository<PostLikeCount, Long> {
-}
-
