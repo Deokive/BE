58339112ce1ffd1590ebc3529d51e8a8d863e197
송성호 <thdtjdgh1679@naver.com>
2026-01-06 23:51:29 +0900

    refactor: FriendQueryRepository -> Slice 구조로 리팩토링에 따른 수정 및 cursorCondition 메서드 추가

--- src/main/java/com/depth/deokive/domain/friend/repository/FriendQueryRepository.java
+++ src/main/java/com/depth/deokive/domain/friend/repository/FriendQueryRepository.java
@@ -3,7 +3,7 @@ package com.depth.deokive.domain.friend.repository;
 import com.depth.deokive.domain.friend.dto.FriendDto;
 import com.depth.deokive.domain.friend.entity.enums.FriendStatus;
 import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -11,7 +11,9 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
 
 import static com.depth.deokive.domain.friend.entity.QFriendMap.friendMap;
 import static com.depth.deokive.domain.user.entity.QUser.user;
@@ -25,21 +27,22 @@ public class FriendQueryRepository {
     /**
      * 친구 목록 조회
      */
-    public Slice<FriendDto.Response> findMyFriends(Long userId, Pageable pageable) {
+    public Slice<FriendDto.Response> findMyFriends(Long userId, Long lastFriendId, LocalDateTime lastAcceptedAt, Pageable pageable) {
 
         List<FriendDto.Response> content = queryFactory
                 .select(Projections.constructor(FriendDto.Response.class,
                         friendMap.friend.id,
-                        friendMap.friend.nickname
+                        friendMap.friend.nickname,
+                        friendMap.acceptedAt
                 ))
                 .from(friendMap)
                 .join(friendMap.friend, user)
                 .where(
                         friendMap.user.id.eq(userId),
-                        friendMap.friendStatus.eq(FriendStatus.ACCEPTED)
+                        friendMap.friendStatus.eq(FriendStatus.ACCEPTED),
+                        cursorCondition(lastFriendId, lastAcceptedAt)
                 )
-                .orderBy(friendMap.acceptedAt.desc()) // 최신순으로 -> 추후 수정
-                .offset(pageable.getOffset())
+                .orderBy(friendMap.acceptedAt.desc(), friendMap.friendId.desc()) // 최신순으로 -> 추후 수정
                 .limit(pageable.getPageSize() + 1)
                 .fetch();
 
@@ -47,7 +50,6 @@ public class FriendQueryRepository {
     }
 
     // --- Helper Methods ---
-
     private <T> Slice<T> checkLastPage(Pageable pageable, List<T> results) {
         boolean hasNext = false;
         if (results.size() > pageable.getPageSize()) {
@@ -55,5 +57,18 @@ public class FriendQueryRepository {
             results.remove(pageable.getPageSize());
         }
         return new SliceImpl<>(results, pageable, hasNext);
+    }
+
+    // 커서 조건
+    private BooleanExpression cursorCondition(Long lastFriendId, LocalDateTime lastAcceptedAt) {
+        if (lastFriendId == null || lastAcceptedAt == null) {
+            return null;
+        }
+
+        // 수락 시간
+        return friendMap.acceptedAt.lt(lastAcceptedAt)
+                //시간이 같으면 id가 더 작은 값으로
+                .or(friendMap.acceptedAt.eq(lastAcceptedAt)
+                        .and(friendMap.friendId.lt(lastFriendId)));
     }
 }
