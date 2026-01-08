1a96cdc27a11bfbad01c937a7b99afa37a7d072e
Hooby <hooby11105@naver.com>
2026-01-09 05:10:07 +0900

    feat: StickerRepository->add methods

--- src/main/java/com/depth/deokive/domain/sticker/repository/StickerRepository.java
+++ src/main/java/com/depth/deokive/domain/sticker/repository/StickerRepository.java
@@ -6,8 +6,16 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface StickerRepository extends JpaRepository<Sticker, Long> {
     @Modifying
     @Query("DELETE FROM Sticker s WHERE s.archive.id = :archiveId")
     void deleteByArchiveId(@Param("archiveId") Long archiveId);
+
+    // 🧐 Event의 경우와 다른데요? 그때는 시간을 다룸 (LocalDateTime) -> 정확성 이슈가 있었음
+    List<Sticker> findAllByArchiveIdAndDateBetweenOrderByDateAsc(Long archiveId, LocalDate startDate, LocalDate endDate);
+
+    boolean existsByArchiveIdAndDate(Long archiveId, LocalDate date);
 }
