839768e09be0a0084fe27f09283e15a4ebffd7b7
송성호 <thdtjdgh1679@naver.com>
2026-01-11 19:04:00 +0900

    refactor: CommentQueryRepository -> Slice방식으로 수정

--- src/main/java/com/depth/deokive/domain/comment/repository/CommentQueryRepository.java
+++ src/main/java/com/depth/deokive/domain/comment/repository/CommentQueryRepository.java
@@ -1,10 +1,15 @@
 package com.depth.deokive.domain.comment.repository;
 
 import com.depth.deokive.domain.comment.entity.Comment;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.List;
 
 
@@ -20,17 +25,50 @@ public class CommentQueryRepository {
     /**
      * 특정 게시글의 댓글조회
      */
-    public List<Comment> findAllByPostId(Long postId) {
-        return queryFactory
+    public Slice<Comment> findAllByPostId(Long postId, Long lastCommentId, Pageable pageable) {
+
+        // SEQ 1. 부모 ID 조회
+        List<Long> parentIds = queryFactory
+                .select(comment.id)
+                .from(comment)
+                .where(
+                        comment.post.id.eq(postId),
+                        comment.parent.isNull(),
+                        gtCommentId(lastCommentId)
+                )
+                .orderBy(comment.id.asc())
+                .limit(pageable.getPageSize() + 1)
+                .fetch();
+
+        if (parentIds.isEmpty()) {
+            return new SliceImpl<>(Collections.emptyList(), pageable, false);
+        }
+
+        boolean hasNext = false;
+        if (parentIds.size() > pageable.getPageSize()) {
+            hasNext = true;
+            parentIds.remove(parentIds.size() - 1);
+        }
+
+        // SEQ 2. 부모, 자식 조회
+        List<Comment> comments = queryFactory
                 .selectFrom(comment)
-                .leftJoin(comment.user, user).fetchJoin()
-                .where(comment.post.id.eq(postId))
+                .leftJoin(comment.user, user)
+                .fetchJoin()
+                .where(
+                        comment.parent.id.in(parentIds)
+                                .or(comment.id.in(parentIds))
+                )
                 .orderBy(
-                        // 부모(NULL 먼저) -> 자식
-                        comment.parent.id.asc().nullsFirst(),
-                        // 시간순 정렬
+                        comment.parent.id.coalesce(comment.id).asc(),
                         comment.createdAt.asc()
                 )
                 .fetch();
+
+        return new SliceImpl<>(comments, pageable, hasNext);
+    }
+
+    private BooleanExpression gtCommentId(Long lastCommentId) {
+        return lastCommentId == null ? null : comment.id.gt(lastCommentId);
     }
 }
