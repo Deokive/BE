c65aca1607a1939623d67792011c375af1177f70
Hooby <hooby11105@naver.com>
2026-01-14 13:03:24 +0900

    feat: UserLoadService->add Cache for retrieving performance

--- src/main/java/com/depth/deokive/system/security/util/UserLoadService.java
+++ src/main/java/com/depth/deokive/system/security/util/UserLoadService.java
@@ -5,6 +5,7 @@ import com.depth.deokive.domain.user.repository.UserRepository;
 import com.depth.deokive.system.security.model.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,16 +17,19 @@ import java.util.Optional;
 public class UserLoadService {
     private final UserRepository userRepository;
 
+    // sync = true 설정 시, 여러 스레드가 동시에 접근해도 한 명만 DB에 가고 나머지는 첫 번째 스레드가 채워줄 때까지 대기함
+    @Cacheable(value = "userPrincipal", key = "#userId", sync = true)
     @Transactional(readOnly = true)
     public Optional<UserPrincipal> loadUserById(Long userId) {
-        log.info("🟢 UserLoadService loadUserById called with");
+        log.info("🟢 UserLoadService loadUserById called with"); // 이 로그가 찍히면 실제 DB에 쿼리가 나가는 상황 (Cache Miss)
         return userRepository.findById(userId)
                 .map(UserPrincipal::from);
     }
 
+    @Cacheable(value = "userPrincipal", key = "#username", sync = true) // unless = "#result == null"
     @Transactional(readOnly = true)
     public Optional<UserPrincipal> loadUserByUsername(String username) {
-        log.info("🟢 UserLoadService loadUserByUsername called with");
+        log.info("🟢 UserLoadService loadUserByUsername called with"); // 이 로그가 찍히면 실제 DB에 쿼리가 나가는 상황 (Cache Miss)
         Optional<User> user = userRepository.findByUsername(username);
         return user.map(UserPrincipal::from);
     }
