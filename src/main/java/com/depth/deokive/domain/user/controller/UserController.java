fe5dda16f3adfff839c24b795e2fa0c2e02032f6
송성호 <thdtjdgh1679@naver.com>
2026-01-17 20:51:19 +0900

    chore: UserController -> User Swagger 명세 추가

--- src/main/java/com/depth/deokive/domain/user/controller/UserController.java
+++ src/main/java/com/depth/deokive/domain/user/controller/UserController.java
@@ -6,11 +6,15 @@ import com.depth.deokive.system.security.model.UserPrincipal;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,7 +27,10 @@ public class UserController {
 
     @GetMapping("/me")
     @Operation(summary = "내 정보 조회", description = "현재 로그인된 사용자의 정보를 조회합니다.")
-    @ApiResponse(responseCode = "200", description = "내 정보 조회 성공")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "내 정보 조회 성공"),
+            @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     public UserDto.UserResponse retrieve(
             @Parameter(hidden = true) @AuthenticationPrincipal UserPrincipal userPrincipal
     ) {
@@ -32,7 +39,11 @@ public class UserController {
 
     @PatchMapping("/me")
     @Operation(summary = "내 정보 수정", description = "현재 로그인된 사용자의 정보를 수정합니다.")
-    @ApiResponse(responseCode = "200", description = "내 정보 수정 성공")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "내 정보 수정 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 (유효성 검사 실패)", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     public UserDto.UserResponse update(
             @Parameter(hidden = true) @AuthenticationPrincipal UserPrincipal userPrincipal,
             @Valid @RequestBody UserDto.UserUpdateRequest request
